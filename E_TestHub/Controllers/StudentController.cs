d439cc4018245847b7b2d18ffff9321f9e6c9961
TienxDun <23DH110578@st.huflit.edu.vn>
2025-09-28 22:06:17 +0700

    Page Added: MyExams

--- E_TestHub/Controllers/StudentController.cs
+++ E_TestHub/Controllers/StudentController.cs
@@ -14,12 +14,7 @@ namespace E_TestHub.Controllers
             return View();
         }
 
-        public IActionResult ExamSchedule()
-        {
-            return View();
-        }
-
-        public IActionResult ExamHistory()
+        public IActionResult MyExams()
         {
             return View();
         }
@@ -27,6 +22,44 @@ namespace E_TestHub.Controllers
         public IActionResult ExamInfo(int examId)
         {
             ViewBag.ExamId = examId;
+            
+            // Simulate exam status based on current date and exam schedule
+            var currentDate = new DateTime(2025, 9, 28); // Fixed current date for demo
+            string examStatus = "upcoming"; // Default
+            
+            // Mock exam data with different statuses
+            switch (examId)
+            {
+                case 1: // ReactJS exam - in progress today (28/9/2025)
+                    var examDate1 = new DateTime(2025, 9, 28);
+                    var examEndTime1 = examDate1.AddHours(2); // Exam duration 2 hours
+                    
+                    if (currentDate.Date == examDate1.Date && currentDate <= examEndTime1)
+                        examStatus = "in-progress";
+                    else if (currentDate > examEndTime1)
+                        examStatus = "completed";
+                    else
+                        examStatus = "upcoming";
+                    break;
+                    
+                case 2: // History exam - upcoming (5/10/2025)
+                    var examDate2 = new DateTime(2025, 10, 5);
+                    var examEndTime2 = examDate2.AddHours(2);
+                    
+                    if (currentDate.Date == examDate2.Date && currentDate <= examEndTime2)
+                        examStatus = "in-progress";
+                    else if (currentDate > examEndTime2)
+                        examStatus = "completed";
+                    else
+                        examStatus = "upcoming";
+                    break;
+                    
+                default:
+                    examStatus = "upcoming";
+                    break;
+            }
+            
+            ViewBag.ExamStatus = examStatus;
             return View();
         }
 
